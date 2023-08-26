3d8b7d8cf5a339bb61482d8b32b756ccaab4599d
Tesfaye <contctesfaye@gmail.com>
2023-08-26 14:29:24 +0300

    files modified

--- main.js
+++ main.js
@@ -10,3 +10,40 @@ navmob.addEventListener("click", () => {
     navmob.classList.remove("nav--open");
     hamburger.classList.remove("hamburger--open");
 });
+
+const body = document.querySelector('body');
+const readmore = document.querySelector('.btn-read-on');
+
+
+
+readmore.addEventListener('click', ()=>{
+    const popup = document.createElement('section');
+    popup.className = "popwin";
+    const popcontent = document.createElement('div');
+    popcontent.className = "popcontent";
+    popcontent.innerHTML = `<button type="button" class="close">close</button>
+    <h1>
+    The project office Finalized the following Projects...
+        The project office Finalized the following Projects...
+        The project office Finalized the following Projects...
+        The project office Finalized the following Projects...
+        The project office Finalized the following Projects...
+        The project office Finalized the following Projects...
+        The project office Finalized the following Projects...
+        The project office Finalized the following Projects...
+        The project office Finalized the following Projects...
+        The project office Finalized the following Projects...
+        The project office Finalized the following Projects....</h1>`;
+           body.appendChild(popup);
+popup.appendChild(popcontent);
+
+const close= document.querySelector('.close');
+close.addEventListener('click', ()=> {
+   body.removeChild(popup,popcontent);
+
+})
+
+
+})
+
+
